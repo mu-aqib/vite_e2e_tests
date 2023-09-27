5f7942bf5a741ca5068dc1f51dc1d60213bd1527
mu-aqib <aqibafridi0345@gmail.com>
2023-09-27 13:57:42 +0500

    notification test cases for each status

--- src/components/__tests__/Notification.test.js
+++ src/components/__tests__/Notification.test.js
@@ -4,6 +4,7 @@ import { mount } from '@vue/test-utils'
 import NotificationToast from '../NotificationToast.vue'
 
 describe('NotificationToast', () => {
+//   test for error message
   test('renders notification style properly for error', () => {
     const status = "error"
     const elementWrapper = mount(NotificationToast, {
@@ -14,4 +15,29 @@ describe('NotificationToast', () => {
 
     expect(elementWrapper.classes()).toEqual(expect.arrayContaining(['notification--error']))
   })
+  
+//   test for success message
+  test('renders notification style properly for success', () => {
+    const status = "success"
+    const elementWrapper = mount(NotificationToast, {
+        props: {
+            status
+        }
+    })
+
+    expect(elementWrapper.classes()).toEqual(expect.arrayContaining(['notification--success']))
+  })
+
+  //   test for info message
+  test('renders notification style properly for info', () => {
+    const status = "info"
+    const elementWrapper = mount(NotificationToast, {
+        props: {
+            status
+        }
+    })
+
+    expect(elementWrapper.classes()).toEqual(expect.arrayContaining(['notification--info']))
+  })
+
 })
