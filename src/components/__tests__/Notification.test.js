e66bf77f57bea100c2fd9172d749e25bd8038fa2
mu-aqib <aqibafridi0345@gmail.com>
2023-09-27 13:53:11 +0500

    notification first test added

--- src/components/__tests__/Notification.test.js
+++ src/components/__tests__/Notification.test.js
@@ -4,8 +4,14 @@ import { mount } from '@vue/test-utils'
 import NotificationToast from '../NotificationToast.vue'
 
 describe('NotificationToast', () => {
-  test('renders notification properly', () => {
-    const wrapper = mount(NotificationToast, { props: { msg: 'Hello Vitest' } })
-    expect(wrapper.text()).toContain('Hello Vitest')
+  test('renders notification style properly for error', () => {
+    const status = "error"
+    const elementWrapper = mount(NotificationToast, {
+        props: {
+            status
+        }
+    })
+
+    expect(elementWrapper.classes()).toEqual(expect.arrayContaining(['notification--error']))
   })
 })
