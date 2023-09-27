1ff0fcad586fba3c4b01f03f5c29910386b50b33
mu-aqib <aqibafridi0345@gmail.com>
2023-09-27 16:15:08 +0500

    Test cases for Notification compnent completed

--- src/components/__tests__/Notification.test.js
+++ src/components/__tests__/Notification.test.js
@@ -40,4 +40,32 @@ describe('NotificationToast', () => {
     expect(elementWrapper.classes()).toEqual(expect.arrayContaining(['notification--info']))
   })
 
+  //   check the slide up test case
+  test('norification slide up when the message is empty', ()=>{
+    const message = ''
+    const elementWrapper = mount(NotificationToast, {
+        props: {
+            message
+        }
+    })
+    expect(elementWrapper.classes('notification--slide')).toBe(false)
+  })
+
+  //   test case for event to the functionality
+  test('emit event when close the button', async ()=>{
+    const elementWrapper = mount(NotificationToast, {
+        data() {
+            return {
+                isClicked: false
+            }
+        }
+    })
+
+    // const button = elementWrapper.find('button')
+    await elementWrapper.find('button').trigger('click')
+
+    expect(elementWrapper.emitted()).toHaveProperty('clear-notification')
+  })
+
+
 })
